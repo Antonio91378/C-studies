ae68c9d97099c4983a68e17369217b3641c3dfb8
Antonio Dias <antonio.dias@bit.com.br>
2022-06-10 14:51:24 -0300

    first commit

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titularNome;
+        public string titularCpf;
+        public string titularProfissao;
+        public string conta;
+        public int numeroAgencia;
+        public string nomeAgencia;
+        public double saldo = 10000.99;
+
+        public bool Sacar(double valor)
+        {
+            if (0 > valor && valor > saldo)
+            {
+                return false;
+                Console.WriteLine("Error: Valor inválido");
+            }
+            else if (0 < valor && valor < saldo)
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+            else
+            {
+                return false;
+                Console.WriteLine("Error: Valor inválido");
+            }
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo > valor && valor > 0)
+            {
+                saldo -= valor;
+                contaDestino.saldo += valor;
+            }
+            else
+            {
+                Console.WriteLine("Error: Valor inválido");
+            }
+        }
+
+        public static void Depositar(double valor, ContaCorrente contaDestino)
+        {
+
+            if (valor > 0)
+            {
+                contaDestino.saldo += valor;
+            }
+            else
+            {
+                Console.WriteLine("Error: Valor inválido");
+            }
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,28 @@
+﻿using bytebank;
+
+Console.WriteLine("Boas vindas ao seu banco, ByteBank");
+
+//ContaCorrente conta1 = new ContaCorrente();
+//conta1.titularNome = "Antônio Víctor";
+//conta1.conta = "115200-B";
+//conta1.numeroAgencia = 1023;
+//conta1.nomeAgencia = "Agência central";
+
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titularNome = "Clarice";
+//conta2.conta = "9600-B";
+//conta2.numeroAgencia = 255;
+//conta2.nomeAgencia = "Agência central";
+//conta2.saldo = 100.99;
+
+//ContaCorrente.Depositar(500, conta2);
+//conta2.Transferir(500, conta1);
+
+//Console.WriteLine(conta2.saldo);
+//Console.WriteLine(conta1.saldo);
+
+Cliente cliente = new Cliente();
+cliente.Nome = "Antônio Víctor Gonçalves Dias";
+cliente.Cpf = "14354546699";
+cliente.Profissao = "Programador";
